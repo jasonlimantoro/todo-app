320a96f1392bf3eff981250cdab60426a2bb5a1c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-02 00:04:24 +0700

    able to persist state (todos key) to Browser

--- src/store.js
+++ src/store.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import Reducer from "./reducers/Reducer";
+import { loadState, saveState } from "./localStorage";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const persistedState = {
-	todos : [{
-		id : 0,
-		name: 'Start building react-redux application',
-		completed: false,
-	}]
-};
+const persistedState = loadState();
 
 const configureStore = () => {
 
@@ -18,6 +13,9 @@ const configureStore = () => {
 		persistedState,
 		composeWithDevTools(applyMiddleware(thunk)),
 	);
+	store.subscribe(() => {
+		saveState({ todos: store.getState().todos });
+	});	
 
 	return store;
 }
