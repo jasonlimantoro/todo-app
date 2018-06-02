efbfe8a647a94f2dd3e8bea146d88a3c5dfd88ef
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-02 10:55:13 +0700

    remove unused function

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -8,14 +8,3 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
 }
 
 export default visibilityFilter;
-
-export const getVisibleTodos = (state, filter) => {
-	switch (filter) {
-		case "SHOW_COMPLETED":
-			return state.filter(t => t.completed);
-		case "SHOW_INCOMPLETED":
-			return state.filter(t => !t.completed);
-		default:
-			return state;
-	}
-}
